9a7079cc0e99c36980a5583ded89e9905d14becb
Dcy <dcyap0@icloud.com>
2020-09-02 18:24:44 +0800

    Add GlobalValues

--- venv/Include/GlobalValues.py
+++ venv/Include/GlobalValues.py
@@ -1,1 +1,20 @@
+from email.mime.text import MIMEText
 price = 0
+smtpserver = 'smtp.88.com'
+username = 'BTCMonitor@88.com'
+password='PDhdeFNCnVzuzHWt'
+sender='BTCMonitor@88.com'
+#构造html
+#发送正文中的图片:由于包含未被许可的信息，网易邮箱定义为垃圾邮件，报554 DT:SPM ：<p><img src="cid:image1"></p>
+register_html = """
+    <p style="text-align: center;font-weight:bold;font-size:35px;"><br><br>
+    Hi<br>  
+       恭喜你<br>  
+       成功注册BTCMonitor账户！<br> 
+       <br><br><br><br>
+    </p> 
+"""
+register_subject = '注册成功'
+register_msg = MIMEText(register_html,_subtype='html', _charset='utf-8')
+register_msg['Subject'] = register_subject
+register_msg['From'] = sender
--- venv/Include/Mail.py
+++ venv/Include/Mail.py
@@ -2,34 +2,12 @@ import smtplib
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 from email.mime.image import MIMEImage
-
-smtpserver = 'smtp.88.com'
-username = 'BTCMonitor@88.com'
-password='PDhdeFNCnVzuzHWt'
-sender='BTCMonitor@88.com'
-receiver=['dcyap0@icloud.com']
-
-subject = '注册成功'
-
-
-#构造html
-#发送正文中的图片:由于包含未被许可的信息，网易邮箱定义为垃圾邮件，报554 DT:SPM ：<p><img src="cid:image1"></p>
-html = """
-    <p style="text-align: center;font-weight:bold;font-size:35px;"><br><br>
-    Hi<br>  
-       恭喜你<br>  
-       成功注册BTCMonitor账户！<br> 
-       <br><br><br><br>
-    </p> 
-"""
-msg = MIMEText(html,_subtype='html', _charset='utf-8')
-msg['Subject'] = subject
-msg['From'] = sender
+from Include.GlobalValues import *
 
 def register_mail(receiver):
-    msg['To']=receiver
+    register_msg['To']=receiver
     smtp = smtplib.SMTP()
     smtp.connect(smtpserver)
     smtp.login(username, password)
-    smtp.sendmail(sender, receiver, msg.as_string())
+    smtp.sendmail(sender, receiver, register_msg.as_string())
     smtp.quit()
